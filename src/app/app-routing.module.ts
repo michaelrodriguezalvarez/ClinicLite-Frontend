284dd3867f2de665eb032d8e06db79b2ce0d6bc7
Michael Rodriguez Alvarez <michaelrodriguezalvarez@gmail.com>
2022-05-06 17:00:06 -0400

    Add clinic-details route to AppRouting Module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { UsersComponent } from './users/users.component';
 import { TenantsComponent } from './tenants/tenants.component';
 import { RolesComponent } from 'app/roles/roles.component';
 import { ChangePasswordComponent } from './users/change-password/change-password.component';
+import { ClinicDetailsComponent } from './clinic-details/clinic-details.component';
 
 @NgModule({
     imports: [
@@ -16,7 +17,8 @@ import { ChangePasswordComponent } from './users/change-password/change-password
                 path: '',
                 component: AppComponent,
                 children: [
-                    { path: 'home', component: HomeComponent,  canActivate: [AppRouteGuard] },
+                    { path: 'home', component: HomeComponent, canActivate: [AppRouteGuard] },
+                    { path: 'clinic-details', component: ClinicDetailsComponent, data: { permission: 'Pages.ClinicDetails' }, canActivate: [AppRouteGuard] },
                     { path: 'users', component: UsersComponent, data: { permission: 'Pages.Users' }, canActivate: [AppRouteGuard] },
                     { path: 'roles', component: RolesComponent, data: { permission: 'Pages.Roles' }, canActivate: [AppRouteGuard] },
                     { path: 'tenants', component: TenantsComponent, data: { permission: 'Pages.Tenants' }, canActivate: [AppRouteGuard] },
